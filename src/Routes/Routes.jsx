26da91c81386386b4922191c75d8e8a6aee016b2
Sha-hed <shahedcse14@gmail.com>
2024-05-10 01:40:28 +0600

    Project Setup Completed

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Layout/Root";
+import ErrorPage from "../Pages/ErrorPage";
+import Home from "../Pages/Home";
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    },
+]);
