d7c4743959234d810c83f6ec55f96d3fec67118a
Sha-hed <shahedcse14@gmail.com>
2024-05-10 02:00:03 +0600

    Firebase Setting Completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 
 
 const Home = () => {
+
+    console.log("FUCK YOU SHHAAED");
     return (
         <div>
-            <h1>This is From Home Page Man!</h1>
+            <h1>This is From Home Page Man Can You Fuck Hard?</h1>
         </div>
     );
 };
